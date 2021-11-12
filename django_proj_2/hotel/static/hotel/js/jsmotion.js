859cb079408f3535dcd8304fba7c5e20bcdc5c9d
eddiedibs <eduFlynn@hotmail.com>
2021-11-11 23:21:29 -0400

    First commit

---
+++ django_proj_2/hotel/static/hotel/js/jsmotion.js
@@ -0,0 +1,25 @@
+const navMotion = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav__list');
+    const lists = document.querySelectorAll('.nav__list li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        lists.forEach((link, index) => {
+            if (link.style.animation){
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navFadeLists 1.5s ease forwards ${index / 2.5}s`;
+                console.log(index);
+            }
+            
+        });
+    });
+    
+
+    
+
+};
+
+navMotion();
