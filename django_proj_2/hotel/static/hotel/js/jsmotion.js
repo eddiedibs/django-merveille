882a3b4e282dfe3468fe8976084e144d13a25dc2
eddiedibs <eduFlynn@hotmail.com>
2022-02-19 10:57:19 -0400

    Improvements on first slide, js implementation now allows it to slide images automatically through a function, button for activating and deactivating is still yet to be implemented, more code cleaning done aswell.

--- django_proj_2/hotel/static/hotel/js/jsmotion.js
+++ django_proj_2/hotel/static/hotel/js/jsmotion.js
@@ -25,7 +25,7 @@
 
 /*
 ===============================================================
-                        2. navMotion
+                        1. navMotion
 
           Function that shows a slidable menu for PC
 
@@ -75,7 +75,7 @@ const navMotion = () => {
 
 /*
 ===============================================================
-                        4. navChangeColor 
+                        2. navChangeColor 
 
     Function that turns navBar visible when scrolled (PC)
 
@@ -119,7 +119,7 @@ function navChangeColor(){
 
 /*
 ===============================================================
-                        5. arrowMotion 
+                        3. arrowMotion 
 
     Function that allows arrows in main page to slide among images
 
@@ -129,55 +129,172 @@ function navChangeColor(){
 
 
 function arrowMotion(){
-    const rightArrow = document.querySelector('.right-arrow');
-    const leftArrow = document.querySelector('.left-arrow');
-    const slides = document.querySelector('.slides')
-    const slidesImg = document.querySelectorAll('.slides__img')
 
-    /*
-           [DEPRECATED]
+   // Banner slide function
+    window.onload = function () {
+        const helpers = (function () {
+          function getDOMElements(DOMSelectors) {
+            let DOMElements = {};
+            for (let selector in DOMSelectors) {
+              if (DOMSelectors.hasOwnProperty(selector)) {
+                DOMElements[selector] = document.querySelector(
+                  DOMSelectors[selector]
+                );
+              }
+            }
+            return DOMElements;
+          }
+          return {
+            getDOMElements
+          };
+        })();
+      
+        const modal = (function () {
+          const state = {
+            counter: 0,
+            intervalID: 0
+          };
+          let CONSTANTS = {
+            ACTIVE_CLASS_NAME: "active",
+            TIMER: 5000,
+            TRANSITION: "all .6s ease-out"
+          };
+          function addConstant(key, value) {
+            CONSTANTS[key] = value;
+          }
+      
+          return {
+            state,
+            CONSTANTS,
+            addConstant
+          };
+        })();
+      
+        const view = (function (helpers) {
+          const DOMSelectors = {
+            carouselInnerSlider: ".slides",
+            slide: "#slide",
+            prevButton: ".left-arrow",
+            nextButton: ".right-arrow",
+            carouselImages: ".slides-img-container > img",
+          };
+          const DOMElements = helpers.getDOMElements(DOMSelectors);
+          const CAROUSEL_IMAGES = [
+            ...document.querySelectorAll(DOMSelectors.carouselImages)
+          ];
+          function moveSliderToIndex(IMAGE_SIZE, index) {
+            DOMElements.carouselInnerSlider.style.transform = `translateX(-${
+              IMAGE_SIZE * index
+            }px)`;
+          }
+          function addClassToIndex(className, index) {
+            CAROUSEL_IMAGES[index].classList.add(className);
+          }
+          function removeClassToIndex(className, index) {
+            CAROUSEL_IMAGES[index].classList.remove(className);
+          }
+       
+       
+          function setTransition(element, transition) {
+            element.style.transition = `${transition}`;
+          }
+          return {
+            DOMSelectors,
+            moveSliderToIndex,
+            addClassToIndex,
+            removeClassToIndex,
+            setTransition
+          };
+        })(helpers);
+      
+        const controller = (function (modal, view, helpers) {
+          const DOMSelectors = view.DOMSelectors;
+          const DOMElements = helpers.getDOMElements(DOMSelectors);
+          
+          function initApp() {
+            const imageSize = DOMElements.carouselInnerSlider.clientWidth;
+            // DOMElements.carouselImages.style.margin = `${1.1 + 6}`;
+            const imagesCount =
+              [...document.querySelectorAll(DOMSelectors.carouselImages)].length - 1;
+            modal.addConstant("IMAGE_SIZE", imageSize);
+            modal.addConstant("TOTAL_IMAGES", imagesCount);
+            view.moveSliderToIndex(modal.CONSTANTS.IMAGE_SIZE, modal.state.counter);
+            handleAdding();
+            DOMElements.nextButton.addEventListener("click", handleNextImage);
+            DOMElements.prevButton.addEventListener("click", handlePrevImage);
+          }
+          function removeEventListeners() {
+            DOMElements.nextButton.removeEventListener("click", handleNextImage);
+            DOMElements.prevButton.removeEventListener("click", handlePrevImage);
+          }
+          function handleNextImage() {
+            handleRemove();
+            if (modal.state.counter === modal.CONSTANTS.TOTAL_IMAGES) {
+              modal.state.counter = -1;
+            }
+            modal.state.counter += 1;
+            handleAdding();
+            view.moveSliderToIndex(modal.CONSTANTS.IMAGE_SIZE, modal.state.counter);
+          }
+          function handlePrevImage() {
+            handleRemove();
+            if (modal.state.counter === 0) {
+              modal.state.counter = modal.CONSTANTS.TOTAL_IMAGES + 1;
+            }
+            modal.state.counter -= 1;
+            handleAdding();
+            view.moveSliderToIndex(modal.CONSTANTS.IMAGE_SIZE, modal.state.counter);
+          }
+         
+          function handleSlide() {
+            const isChecked = true;
+            if (isChecked) {
+              modal.state.intervalID = setInterval(() => {
+                handleNextImage();
+              }, modal.CONSTANTS.TIMER);
+            } else {
+              clearInterval(modal.state.intervalID);
+              modal.state.intervalID = null;
+            }
 
-        NOTE: slidesImg is not used in this version
-
-        slidesImg[0] = 0
-        slidesImg[1] = 100%
-        slidesImg[2] = 200%
-        
-
-
-    */
-
-    // For slide Arrows
-    let currentSlide = [0, 100, 200, 300];
-
-    rightArrow.addEventListener('click', ()=> {
-        goToSlide(currentSlide[1]);
-            rightArrow.addEventListener('click', ()=> {
-                goToSlide(currentSlide[2]);
-                rightArrow.addEventListener('click', ()=> {
-                    goToSlide(currentSlide[2]);
-                    
-                })
-            })
-            leftArrow.addEventListener('click', () => {
-                goToSlide(currentSlide[1]);
-                leftArrow.addEventListener('click', () => {
-                    goToSlide(currentSlide[0]);
-                    rightArrow.addEventListener('click', ()=> {
-                        goToSlide(currentSlide[1]);
-                    })
-                    
-                })
             
-            })
-    })
+          }
+          handleSlide();
 
-    function goToSlide(slideNumber){
-        slides.style.marginLeft = "-" + slideNumber + "%";
-    }
-
-
-  
+          function handleRemove() {
+            view.removeClassToIndex(
+              modal.CONSTANTS.ACTIVE_CLASS_NAME,
+              modal.state.counter
+            );
+          }
+          function handleAdding() {
+            view.addClassToIndex(
+              modal.CONSTANTS.ACTIVE_CLASS_NAME,
+              modal.state.counter
+            );
+          }
+          DOMElements.carouselInnerSlider.addEventListener(
+            "transitionstart",
+            removeEventListeners
+          );
+          DOMElements.carouselInnerSlider.addEventListener("transitionend", initApp);
+          view.setTransition(
+            DOMElements.carouselInnerSlider,
+            modal.CONSTANTS.TRANSITION
+          );
+          return {
+            initApp,
+            removeEventListeners
+          };
+        })(modal, view, helpers);
+      
+        controller.initApp();
+      
+        window.addEventListener("resize", () => {
+          controller.removeEventListeners();
+          controller.initApp();
+        });
+      };
 
 
 
@@ -186,7 +303,7 @@ function arrowMotion(){
 
 /*
 =====================================================================
-                        6. arrowMotion2 
+                        4. arrowMotion2 
 
     Function that allows arrows in about page to slide among images
                         MADE WITH JQUERY
@@ -242,7 +359,7 @@ function arrowMotion2(){
 
 /*
 =====================================================================
-                        7. serviceActive 
+                        5. serviceActive 
 
     Function that allows service section to disappear typography and
                     to show logo MADE WITH JQUERY
@@ -316,7 +433,7 @@ function serviceActive(){
 
 /*
 ===============================================================
-                        8. mainApp 
+                        6. mainApp 
 
     Function that activates all of them in one main function
 
